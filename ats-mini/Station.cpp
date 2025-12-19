ea6f2b75ee59c1f40be2acc612a377b44148605d
neurici <neurici@gmail.com>
2025-12-19 13:27:54 +0200

    Update Station.cpp

--- ats-mini/Station.cpp
+++ ats-mini/Station.cpp
@@ -42,26 +42,26 @@ static const char *cbChannelNumber[] =
 //
 const char *rdsProgramTypes[32] =
 {
-  0, "News", "Current Affairs", "Information",
-  "Sport", "Education", "Drama", "Culture",
-  "Science", "Varied", "Pop Music", "Rock Music",
-  "Easy Listening", "Light Classical", "Serious Classical", "Other Music",
-  "Weather", "Finance", "Children's Program", "Social Affairs",
-  "Religion", "Phone-In", "Travel", "Leisure",
-  "Jazz Music", "Country Music", "National Music", "Oldies Music",
-  "Folk Music", "Documentary", "TEST", "! ALERT !"
+  0, "Stiri", "Actualitati", "Informatii",
+  "Sport", "Educatie", "Drama", "Cultura",
+  "Stiinta", "Variat", "Muzica Pop", "Muzica Rock",
+  "Muzica Usoara", "Clasica Usoara", "Clasica Serioasa", "Alta Muzica",
+  "Vremea", "Finante", "Program pentru Copii", "Afacerii Sociale",
+  "Religie", "Emisiune Telefonica", "Calatorii", "Timp Liber",
+  "Muzica Jazz", "Muzica Country", "Muzica Nationala", "Muzica Veche",
+  "Muzica Folk", "Documentar", "TEST", "! ALERTA !"
 };
 
 const char *rbdsProgramTypes[32] =
 {
-  0, "News", "Information", "Sports",
-  "Talk", "Rock", "Classic Rock", "Adult Hits",
-  "Soft Rock", "Top 40", "Country", "Oldies",
-  "Soft Music", "Nostalgia", "Jazz", "Classical",
-  "R & B", "Soft R & B", "Foreign Language", "Religious Music",
-  "Religious Talk", "Personality", "Public", "College",
-  "Spanish Talk", "Spanish Music", "Hip Hop", 0,
-  0, "Weather", "TEST", "! ALERT !"
+  0, "Stiri", "Informatii", "Sport",
+  "Talk", "Rock", "Rock Clasic", "Hituri pentru Adulti",
+  "Rock Lent", "Top 40", "Country", "Muzica Veche",
+  "Muzica Lenta", "Nostalgie", "Jazz", "Clasica",
+  "R & B", "R & B Lent", "Limba Straina", "Muzica Religioasa",
+  "Discutii Religioase", "Personalitate", "Public", "Universitar",
+  "Talk Spaniol", "Muzica Spaniola", "Hip Hop", 0,
+  0, "Vremea", "TEST", "! ALERTA !"
 };
 
 static char bufStationName[50]  = "";
@@ -203,8 +203,8 @@ static bool showRdsTime(const char *rdsTime)
   // If NTP time available, do not use RDS time
   if(!rdsTime || ntpIsAvailable()) return(false);
 
-  // The standard RDS time format is ìHH:MMî.
-  // or sometimes more complex like ìDD.MM.YY,HH:MMî.
+  // The standard RDS time format is ‚ÄúHH:MM‚Äù.
+  // or sometimes more complex like ‚ÄúDD.MM.YY,HH:MM‚Äù.
   const char *timeField = strstr(rdsTime, ":");
 
   // If we find a valid time format...
